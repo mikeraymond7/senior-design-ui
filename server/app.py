610f5c85c897de5079f05a603eeabef878dfbe24
Mike <michael.raymond1@comcast.net>
2022-09-29 23:34:30 -0400

    Simple updates to server files

--- server/app.py
+++ server/app.py
@@ -1,18 +1,10 @@
-'''
-sudo pip install firebase-admin
-pip install --user firebase-admin
-'''
-'''
-To run:
-    FLASK_APP=hello.py flask run
-'''
 import os
 from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore, initialize_app
 
 app = Flask(__name__)
 
-cred = credentials.Certificate('../../../key.json')
+cred = credentials.Certificate('../../key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
 users_ref = db.collection('users')
