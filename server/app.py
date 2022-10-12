f0ff4ea92866f8ef97241ef755d0270bb0146efb
Mike <michael.raymond1@comcast.net>
2022-10-12 11:06:39 -0400

    Update Upload

--- server/app.py
+++ server/app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 cred = credentials.Certificate('../../key.json')
 initialize_app(cred, {'storageBucket': 'groovy-groove-363915.appspot.com'})
 db = firestore.client()
-users_ref = db.collection('users')
-stats_ref = db.collection('stats')
-lifts_ref = db.collection('lifts')
 
 @app.route('/uploader')
 def upload_file():
@@ -17,22 +14,42 @@ def upload_file():
 
 
 # File successfully uploads
-# Need to fully confirm with Download
 # Make it so that files can be uploaded by bytes
 @app.route('/uploadFile', methods=['POST'])
 def uploadFile():
-	f = request.files['file']
-	#f.save(f.filename)
-	try:
-		fileName = f.filename
-		#fileName = "../../test.txt"
-		bucket = storage.bucket()
-		blob = bucket.blob(fileName)
-		print(blob)
-		blob.upload_from_filename(fileName)
-		blob.make_public()
-		print("File URL: " + blob.public_url)
-		return jsonify({"success": True}), 201
-	except Exception as e:
-		return f"An Error Occured while uploading file: {e}."
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(f.filename)
+		try:
+			fileName = f.filename
+			#fileName = "../../test.txt"
+			bucket = storage.bucket()
+			blob = bucket.blob(fileName)
+			print(blob)
+			blob.upload_from_filename(fileName)
+			blob.make_public()
+			print("File URL: " + blob.public_url)
+			os.remove(fileName)
+			return jsonify({"success": True}), 200
+		except Exception as e:
+			# change error to return to error page
+			return f"An Error Occured while uploading file: {e}."
+
+@app.route('/downloadFile', methods=['POST'])
+def downloadFile():
+	#f = request.files['fileName']
+	bucket = storage.bucket()
+	blob = bucket.blob("test.txt")
+	blow.download_to_filename("downloaded.txt")
+	return jsonify({"success" : True}), 200
+
+
+
+
+
+
+
+
+
+
 
